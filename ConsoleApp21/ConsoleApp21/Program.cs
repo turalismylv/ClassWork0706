43351f5cdb417b37e3bfd4983cd653d2cf5f4ded
Tural H. Ismayilov <turalhi@code.local>
2022-06-07 21:41:54 +0400

    CLassWork

---
+++ ConsoleApp21/ConsoleApp21/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shop shop = new Shop();
+            while (true)
+            {
+
+                Console.WriteLine("1.Product elave edin  2.Product satin");
+                string num = Console.ReadLine();
+                bool isInt = int.TryParse(num, out int menu);
+
+                if (isInt)
+                {
+
+
+
+                    switch (menu)
+                    {
+                        
+                        case 1:
+                            Console.WriteLine("Adin daxil edin ");
+                            string name = Console.ReadLine();
+                            Console.WriteLine("qiymet daxil edin");
+                            string price = Console.ReadLine();
+                            double pr = double.Parse(price);
+                            Console.WriteLine("Say daxil edin");
+                            string cnt = Console.ReadLine();
+                            int count = int.Parse(cnt);
+                            if (name.ToUpper()==Type.COFFE.ToString())
+                            {
+                                Coffe cofe = new Coffe(name, count, pr);
+                                shop.Add(cofe);
+                            }
+                            else if (name.ToUpper()==Type.TEA.ToString())
+                            {
+                                Tea tea = new Tea(name, count, pr);
+                                shop.Add(tea);
+
+                            }
+                            break;
+                            
+                           
+                        default:
+                            break;
+                    }
+
+
+
+                }
+
+            }
+            
+        }
+        enum Type { 
+        COFFE,
+        TEA
+        
+        }
+
+    }
+}
+
---
+++ ConsoleApp21/ConsoleApp21/Shop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    
+    class Shop
+    {
+        public double Totalincome { get; set; }
+        private Object[] product;
+        public Shop()
+        {
+            product = new object[0];
+        }
+        public void Add(Object obj)
+        {
+
+            Array.Resize(ref product, product.Length + 1);
+            product[product.Length - 1] = obj;
+            Console.WriteLine("Elave olundu");
+
+        }
+    }
+   
+}
